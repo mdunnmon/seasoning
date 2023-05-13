287e3ff059bf477c5cdd469d6e9a232087d5630a
Mike Dunnmon <99291081+mdunnmon@users.noreply.github.com>
2023-05-13 16:56:52 -0700

    Added footer with current weather

--- client/App.tsx
+++ client/App.tsx
@@ -5,6 +5,7 @@ import Create from './components/Create';
 import RecipeList from './components/RecipeList';
 import IndividualRecipe from './components/IndividualRecipe';
 import NotFound from './components/NotFound';
+import Footer from './components/Footer';
 import { Link, Route, Routes } from 'react-router-dom';
 
 const App: React.FC = () => {
@@ -25,7 +26,7 @@ const App: React.FC = () => {
             <Link to="/recipe">Recipe List</Link>
           </li>
           <a
-            href="https://youtu.be/dQw4w9WgXcQ?t=1"
+            href=""
             className="inline-block text-sm px-4 py-2 leading-none border rounded mt-4 lg:mt-0  text-slate-200 hover:text-white hover:bg-gradient-to-r from-emerald-700 to-emerald-600"
           >
             Log in
@@ -39,6 +40,7 @@ const App: React.FC = () => {
         <Route path="/recipe/:id" element={<IndividualRecipe />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+      <Footer />
     </>
   );
 };
